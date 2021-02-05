036886365f51bfc2c7d2d36556f3f8f7d8f48242
Mikhail <sergeevmisha16@gmail.com>
2021-02-05 12:25:54 +0300

    upd Coroutine Pistol.cs

--- Assets/Scripts/Weapons/Pistol.cs
+++ Assets/Scripts/Weapons/Pistol.cs
@@ -7,15 +7,11 @@ public class Pistol : BaseWeapon
 {
     private LineRenderer lineRenderer;
     private bool ShotLineCreated = false;
+    private Coroutine prevCoroutine;
     public override void Init()
     {
         base.Init();
         lineRenderer = GetComponent<LineRenderer>();
-    }
-
-    private void Update()
-    {
-        StartCoroutine(DisableShotLine(3));
     }
 
     public override void Shoot()
@@ -28,38 +24,40 @@ public class Pistol : BaseWeapon
     {
         RaycastHit hit;
         
-        if (Physics.Raycast(WeaponManager.RayCastPoint.position, transform.forward, out hit, 500f))
+        if (Physics.Raycast(WeaponManager.RayCastPoint.position,WeaponManager.RayCastPoint.forward , out hit, 500f))
         {
             Debug.DrawLine(WeaponManager.RayCastPoint.position, hit.point, Color.black, 2f);
             
             CreateShotLine(transform.position, hit.point, Color.red);
+            
+            print(hit.collider.name);
         }
         else
         {
-            CreateShotLine(transform.position, transform.forward * -1 * 10f, Color.yellow);
+            CreateShotLine(transform.position,  WeaponManager.RayCastPoint.position + WeaponManager.RayCastPoint.forward * 10f, Color.yellow);
             
-            Debug.DrawRay(WeaponManager.RayCastPoint.position, transform.forward * -1 * 10f, Color.magenta, 2f);
+            Debug.DrawRay(WeaponManager.RayCastPoint.position, WeaponManager.RayCastPoint.forward * 10f, Color.magenta, 2f);
         }
     }
 
     private void CreateShotLine(Vector3 startPos, Vector3 endPos, Color color)
     {
+        if(prevCoroutine != null)
+            StopCoroutine(prevCoroutine);
+        
         lineRenderer.enabled = true;
         lineRenderer.SetColors(color, color);
         lineRenderer.SetPosition(0, startPos);
         lineRenderer.SetPosition(1, endPos);
+
+        prevCoroutine = StartCoroutine(DisableShotLine(2));
         
-        ShotLineCreated = true;
     }
 
     private IEnumerator DisableShotLine(float time)
     {
-        if (!ShotLineCreated)
-            yield break;
-        
         yield return new WaitForSeconds(time);
         
         lineRenderer.enabled = false;
-        ShotLineCreated = false;
     }
 }
